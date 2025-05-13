351f856bc42b30956b9c8c3860f881023376746e
Mayzihardiputra <eziee496@gmail.com>
2025-05-13 12:14:35 +0700

    structur node

--- Doublelinked.cpp
+++ Doublelinked.cpp
@@ -1,3 +1,14 @@
 // 0254_DoublelinkedList
 #include <iostream>
 using namespace std;
+
+// membuat struktur untuk object double linkedlist
+struct Node
+{
+    // dekralasi noMhs dan name untuk menampung data
+    int noMhs;
+    string name;
+    // dekralasi pointer next dan pref untuk penunjukan data sebelum dan sesudah
+    Node *next;
+    Node *prev;
+};
