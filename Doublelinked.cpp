6f3503f312b722c5896e85eca5b3c541e234e7c7
Mayzihardiputra <eziee496@gmail.com>
2025-05-13 12:26:27 +0700

    kondisi jika semua kondisi if tidak terpenuhi

--- Doublelinked.cpp
+++ Doublelinked.cpp
@@ -49,3 +49,18 @@ void addNode()
         newNode->prev = NULL; // step 5: make the new node point to NULL
         START = newNode;      // step 6: make the new node the first node
     }
+    // konidisi jika semua kondisi if tidak terpenuhi
+    else
+    {
+
+        // insert the new node in the middle or at the end
+        // set nilai current = start dan nilai previous = null
+        Node *current = START; // step 1.a start from the first node
+        Node *previous = NULL; // step1.b: previous node is Null initially
+
+        // Looping selama current != null dan noMhs dari current lebih kecil dan
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {                            // step 1.c traverse the list to find the
+            previous = current;      // step 1.d: move the previous to the current
+            current = current->next; // step 1.e: move the curernt to the next
+        }
