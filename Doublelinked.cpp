44d3a7ed644d6947614faa2333bf320d13b2ada9
Mayzihardiputra <eziee496@gmail.com>
2025-05-13 12:25:19 +0700

    insert the new node in the list

--- Doublelinked.cpp
+++ Doublelinked.cpp
@@ -37,3 +37,15 @@ void addNode()
             cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
             return;
         }
+        // if the list is empty, make the new node the start
+        // jika list kosong, maka node next nya adalah start
+        newNode->next = START; // step 3: make the new node point to the frst no
+        // kondisi jika start tidak memiliki nilai atau tidak kosong
+        if (START != NULL)
+        {
+            START->prev = newNode; // step 4: make the first node pint to the
+        }
+        // memberi nilai prev = null dan start = node baru
+        newNode->prev = NULL; // step 5: make the new node point to NULL
+        START = newNode;      // step 6: make the new node the first node
+    }
